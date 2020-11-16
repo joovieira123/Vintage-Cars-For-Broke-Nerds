df98a46d937e607c76c39c29022dacf5a5e9ec2f
Joao Vieira <joo.vieira123@gmail.com>
2020-11-16 18:00:31 +0100

    seeded the db with 3 cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Car.new(model: 'Ford Focus', location: 'Sodermalm')
+Car.new(model: 'Rolls Royce', location: 'Gamla Stan')
+Car.new(model: 'Fiat 500', location: 'Normalm')
