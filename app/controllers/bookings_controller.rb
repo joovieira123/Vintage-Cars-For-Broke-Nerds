1bff4ab1d1b4dd1b016df933b5ae5cb07909204d
Joao Vieira <joo.vieira123@gmail.com>
2020-11-18 13:09:59 +0100

    added year, price, description to cars table + date and comments to boookings table and changed all the corresponding models, controlers and views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,39 +1,47 @@
 class BookingsController < ApplicationController
-  def new
-    @car = Car.find(params[:car_id])
-    @booking = Booking.new
-  end
-
-  def create
-    @car = Car.find(params[:car_id])
-    @booking = Booking.new(booking_params)
-    @booking.car = @car
-    @booking.user = current_user
-    if @booking.save
-      redirect_to car_path(@car)
-    else
-      @booking = Booking.new
-      render :new
-    end
-  end
+  before_action :set_car, only: %i[new create]
+  before_action :set_booking, only: %i[show destroy]
 
   def index
     @bookings = Booking.all
   end
 
   def show
-    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.car = @car
+    @booking.user = current_user
+    if @booking.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
   end
 
   def destroy
-    @booking = Booking.find(params[:id])
     @car = @booking.car
     @booking.destroy
 
     redirect_to car_path(@car)
   end
 
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_car
+    @car = Car.find(params[:car_id])
+  end
+
   def booking_params
-    params.require(:booking).permit(:description)
+    params.require(:booking).permit(:comments, :date)
   end
 end
